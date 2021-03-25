94eb6228d631ba2f634c37c89ea56cdaf0015cf8
synesteesia <mikko.ahokas@hotmail.com>
2021-03-25 19:27:36 +0200

    login feature

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask
+from flask import redirect, render_template, request, session
+from os import getenv
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.secret_key = getenv("SECRET_KEY")
+db = SQLAlchemy(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login",methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    sql = "SELECT password FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()    
+    if user == None:
+        # TODO: invalid username warning
+         print("vaara nimi")
+         return redirect("/")
+    else:
+        hash_value = user[0]
+        if check_password_hash(hash_value,password):
+            session["username"] = username
+            return redirect("/")
+        else:
+            # TODO: invalid password warning
+            print("vaara passu")
+            return redirect("/")
+
+@app.route("/createNewAccount",methods=["POST"])
+def createNewAccount():
+    # TODO: check username and password length
+    username = request.form["username"]
+    password = request.form["password"]
+    hash_value = generate_password_hash(password)
+    sql = "INSERT INTO users (username,password) VALUES (:username,:password)"
+    db.session.execute(sql, {"username":username,"password":hash_value})
+    db.session.commit()
+
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
+
+@app.route("/createAccount")
+def createAccount():
+    return render_template("/createAccount.html")
+
+
+    
+
+
+
+
+
+
---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY, 
+  username TEXT, 
+  password TEXT
+  );
+
+  CREATE TABLE messages (
+    id SERIAL PRIMARY KEY, 
+    content TEXT
+    );
+
