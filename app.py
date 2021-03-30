941f45d9b9772bed10927eb6c72f0c8706b1c55c
synesteesia <mikko.ahokas@hotmail.com>
2021-03-30 23:18:58 +0300

    continued coding

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    getrecipes = db.session.execute("SELECT id, recipename FROM recipes WHERE visible=1")
+    recipes = getrecipes.fetchall()
+    return render_template("index.html", recipes=recipes)
 
 @app.route("/login",methods=["POST"])
 def login():
@@ -37,7 +39,7 @@ def login():
 
 @app.route("/createNewAccount",methods=["POST"])
 def createNewAccount():
-    # TODO: check username and password length
+    # TODO: check if username is used and password length
     username = request.form["username"]
     password = request.form["password"]
     hash_value = generate_password_hash(password)
@@ -55,30 +57,117 @@ def logout():
 def createAccount():
     return render_template("/createAccount.html")
 
-@app.route("/recipes")
-def recipes():
-    result = db.session.execute("SELECT id, recipename FROM recipes")
-    recipes = result.fetchall()
-    return render_template("/recipes.html", recipes=recipes)
-
 @app.route("/recipes/<int:id>")
 def recipe(id):
-    sql = "SELECT recipename FROM recipes WHERE id=:id"
+    sql = "SELECT id, recipename FROM recipes WHERE id=:id"
     result = db.session.execute(sql, {"id":id})
-    recipe = result.fetchone()[0]
-    return render_template("recipe.html", id=id, recipe=recipe)
+    recipe = result.fetchone()
+
+    sql= "SELECT id, ingredientname, ingredientamount FROM ingredients WHERE recipe_id=:id"
+    result = db.session.execute(sql, {"id":id})
+    ingredients = result.fetchall()
+
+    sql= "SELECT id, content FROM recipecontents WHERE recipe_id=:id"
+    result = db.session.execute(sql, {"id":id})
+    contents = result.fetchall()
+
+    sql = "SELECT id, content, username FROM messages WHERE recipe_id=:id"
+    result = db.session.execute(sql, {"id":id})
+    messages = result.fetchall()
+
+    return render_template("recipe.html", id=id, recipe=recipe,ingredients=ingredients,contents=contents,messages=messages)
 
 @app.route("/createrecipe")
 def createrecipe():
-    return render_template("/createrecipe.html") 
+    result = db.session.execute("SELECT id, recipename FROM recipes WHERE visible=1")
+    recipes = result.fetchall()
+    return render_template("/createrecipe.html", recipes=recipes) 
 
 @app.route("/createnewrecipe",methods=["POST"])
 def createnewrecipe():
     recipename = request.form["recipename"]
-    sql = "INSERT INTO recipes (recipename) VALUES (:recipename)"
-    db.session.execute(sql, {"recipename":recipename})
+    sql = "INSERT INTO recipes (recipename) VALUES (:recipename) RETURNING id"
+    current = db.session.execute(sql, {"recipename":recipename})
+    result = current.fetchone()
+    id = result[0]
+    db.session.commit()
+    return redirect(f"/recipes/{id}")
+
+@app.route("/deleterecipe/<int:id>")
+def deleterecipe(id):
+    sql = "UPDATE recipes SET visible=0 WHERE id=:id"
+    db.session.execute(sql, {"id":id})
     db.session.commit()
     return redirect("/")
+
+@app.route("/addingredient",methods=["POST"])
+def addIngredient():
+    ingredientname = request.form["ingredientname"]
+    ingredientamount = request.form["ingredientamount"]
+    recipeid = request.form["id"]
+    sql = "INSERT INTO ingredients (ingredientname,ingredientamount,recipe_id) VALUES (:ingredientname,:ingredientamount,:recipeid)"
+    db.session.execute(sql, {"ingredientname":ingredientname,"ingredientamount":ingredientamount,"recipeid":recipeid})
+    db.session.commit()
+    return redirect(f"/recipes/{recipeid}")
+
+@app.route("/deleteingredient/<int:id>")
+def deleteingredient(id):
+    sql = "SELECT recipe_id FROM ingredients WHERE id=:id"
+    current = db.session.execute(sql, {"id":id})
+    result = current.fetchone()
+    recipeid = result[0]
+    sql = "DELETE FROM ingredients WHERE id=:id"
+    db.session.execute(sql, {"id":id})
+    db.session.commit()
+    return redirect(f"/recipes/{recipeid}")
+
+@app.route("/addmessage",methods=["POST"])
+def addmessage():
+    username = request.form["username"]
+    sql = "SELECT id FROM users WHERE username=:username"
+    current = db.session.execute(sql, {"username":username})
+    result = current.fetchone()
+    userid = result[0]
+
+    content = request.form["content"]
+    recipeid = request.form["id"]
+    sql = "INSERT INTO messages (content, username, recipe_id, user_id) VALUES (:content,:username,:recipeid,:userid)"
+    db.session.execute(sql, {"content":content,"username":username,"recipeid":recipeid,"userid":userid})
+    db.session.commit()
+    return redirect(f"/recipes/{recipeid}")
+
+@app.route("/deletemessage/<int:id>")
+def deletemessage(id):
+    sql = "SELECT recipe_id FROM messages WHERE id=:id"
+    current = db.session.execute(sql, {"id":id})
+    result = current.fetchone()
+    recipeid = result[0]
+    sql = "DELETE FROM messages WHERE id=:id"
+    db.session.execute(sql, {"id":id})
+    db.session.commit()
+    return redirect(f"/recipes/{recipeid}")
+
+@app.route("/addcontent",methods=["POST"])
+def addcontent():
+    content = request.form["content"]
+    recipeid = request.form["id"]
+    sql = "INSERT INTO recipecontents (content,recipe_id) VALUES (:content,:recipeid)"
+    db.session.execute(sql, {"content":content,"recipeid":recipeid})
+    db.session.commit()
+    return redirect(f"/recipes/{recipeid}")
+
+@app.route("/deletecontent/<int:id>")
+def deletecontent(id):
+    sql = "SELECT recipe_id FROM recipecontents WHERE id=:id"
+    current = db.session.execute(sql, {"id":id})
+    result = current.fetchone()
+    recipeid = result[0]
+    sql = "DELETE FROM recipecontents WHERE id=:id"
+    db.session.execute(sql, {"id":id})
+    db.session.commit()
+    return redirect(f"/recipes/{recipeid}")
+
+
 
 
 
--- schema.sql
+++ schema.sql
@@ -4,24 +4,30 @@ CREATE TABLE users (
   password TEXT
   );
 
-CREATE TABLE messages (
-  id SERIAL PRIMARY KEY, 
-  content TEXT
-  );
-
 CREATE TABLE recipes (
   id SERIAL PRIMARY KEY,
   recipename TEXT,
+  visible INTEGER DEFAULT 1
   );
 
-CREATE TABLE incredients (
+CREATE TABLE messages (
   id SERIAL PRIMARY KEY, 
-  incredientname TEXT,
-  incredientamount TEXT
+  content TEXT,
+  username TEXT,
+  recipe_id SERIAL REFERENCES recipes,
+  user_id SERIAL REFERENCES users
   );
 
-CREATE TABLE recipeincredients (
-    incredient_id INTEGER REFERENCES incredients,
-    recipe_id SERIAL REFERENCES recipes
+CREATE TABLE ingredients (
+  id SERIAL PRIMARY KEY, 
+  ingredientname TEXT,
+  ingredientamount TEXT,
+  recipe_id SERIAL REFERENCES recipes
+  );
+
+CREATE TABLE recipecontents (
+  id SERIAL PRIMARY KEY, 
+  content TEXT,
+  recipe_id SERIAL REFERENCES recipes
   );
 
